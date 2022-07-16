6a652c90537cb447554723af285006eef44185c7
anthony.phan.co@gmail.com <anthony.phan.co@gmail.com>
2022-07-16 11:33:04 +0800

    enable scripting and fnn

--- install.ps1
+++ install.ps1
@@ -9,4 +9,7 @@ choco install drawio -y
 choco install notepadplusplus -y
 choco install office365business -y
 choco install fnm -y
+set-executionpolicy remotesigned
+New-Item -ItemType Directory -Force -Path $Profile
+"fnm env --use-on-cd | Out-String | Invoke-Expression" >> $Profile
 git config --global user.name "anthony"
